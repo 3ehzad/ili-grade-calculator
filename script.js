5f1bfeefc30b4cd68effdee372378fc744c8e423
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-09-07 15:48:01 +0430

    add: help dialog

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 angular.module('ILIGCapp', ['ngMaterial', 'ngMessages'])
 
-  .controller('gradeController', ['$scope', '$mdToast', function ($scope, $mdToast) {
+  .controller('gradeController', ['$scope', '$mdDialog', '$mdToast', function ($scope, $mdDialog, $mdToast) {
     $scope.calc = function () {
-      $scope.sparray = (new Function("return [" + $scope.sp+ "];")());
+      $scope.sparray = (new Function("return [" + $scope.sp + "];")());
       $scope.spsum = $scope.sparray.reduce((a, b) => a + b, 0);
       $scope.speaking = Number($scope.spsum / $scope.sparray.length) * 4;
 
-      $scope.rdarray = (new Function("return [" + $scope.rd+ "];")());
+      $scope.rdarray = (new Function("return [" + $scope.rd + "];")());
       $scope.rdsum = $scope.rdarray.reduce((a, b) => a + b, 0);
       $scope.reading = Number($scope.rdsum / $scope.rdarray.length) * 3;
 
-      $scope.wrarray = (new Function("return [" + $scope.wr+ "];")());
+      $scope.wrarray = (new Function("return [" + $scope.wr + "];")());
       $scope.wrsum = $scope.wrarray.reduce((a, b) => a + b, 0);
       $scope.writing = Number($scope.wrsum / $scope.wrarray.length) * 1;
 
@@ -24,11 +24,33 @@ angular.module('ILIGCapp', ['ngMaterial', 'ngMessages'])
       //     .hideDelay(7000)
       // );
     }
-
     $scope.reset = function () {
       $scope.sp = '';
       $scope.rd = '';
       $scope.wr = '';
     }
-
+  }])
+    .controller('headController', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+    /* start dialog */
+    $scope.status = '  ';
+    $scope.help = function (ev) {
+      $mdDialog.show({
+        controller: diagController,
+        templateUrl: 'help.tmpl.html',
+        parent: angular.element(document.querySelector('#popupContainer')),
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        fullscreen: true,
+        preserveScope: true
+      })
+    };
+    function diagController($scope, $mdDialog) {
+      $scope.hide = function () {
+        $mdDialog.hide();
+      };
+      $scope.cancel = function () {
+        $mdDialog.cancel();
+      };
+    }
+    /* end dialog */
   }])
