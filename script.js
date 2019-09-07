9af90c25d73636a4f76fd0ef47005b2a40521203
3ehzad <behzad.amiri.nezhad@gmail.com>
2019-09-07 12:48:34 +0430

    clean: some code

--- script.js
+++ script.js
@@ -2,40 +2,27 @@ angular.module('ILIGCapp', ['ngMaterial', 'ngMessages'])
 
   .controller('gradeController', ['$scope', '$mdToast', function ($scope, $mdToast) {
     $scope.calc = function () {
-       // $scope.reading = $scope.rd * 4;
       $scope.sparray = (new Function("return [" + $scope.sp+ "];")());
       $scope.spsum = $scope.sparray.reduce((a, b) => a + b, 0);
-      console.log($scope.spsum);
-      
       $scope.speaking = Number($scope.spsum / $scope.sparray.length) * 4;
-      console.log($scope.speaking);
-      
-      // $scope.reading = $scope.rd * 3;
+
       $scope.rdarray = (new Function("return [" + $scope.rd+ "];")());
       $scope.rdsum = $scope.rdarray.reduce((a, b) => a + b, 0);
-      console.log($scope.rdsum);
-
       $scope.reading = Number($scope.rdsum / $scope.rdarray.length) * 3;
-      console.log($scope.reading);
-     
-      // $scope.writing = $scope.wr * 1;
+
       $scope.wrarray = (new Function("return [" + $scope.wr+ "];")());
       $scope.wrsum = $scope.wrarray.reduce((a, b) => a + b, 0);
-      console.log($scope.wrsum);
-
       $scope.writing = Number($scope.wrsum / $scope.wrarray.length) * 1;
-      console.log($scope.writing);
 
       $scope.allaverage = $scope.speaking + $scope.reading + $scope.writing;
       $scope.finalgrade = Number($scope.allaverage / 8).toFixed(2);
-      console.log($scope.finalgrade);
-      $mdToast.show(
-        $mdToast.simple()
-          .textContent('Class Grade: ' + $scope.finalgrade)
-          .position("top right")
-          .theme('success-toast')
-          .hideDelay(7000)
-      );
+      // $mdToast.show(
+      //   $mdToast.simple()
+      //     .textContent('Class Grade: ' + $scope.finalgrade)
+      //     .position("top right")
+      //     .theme('success-toast')
+      //     .hideDelay(7000)
+      // );
     }
 
     $scope.reset = function () {
