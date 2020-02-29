c655bfe64e7dc74489433e64232f6ee08170515d
3ehzad <behzad.amiri.nezhad@gmail.com>
2020-02-29 18:47:41 +0330

    fix bug

--- elementry/script.js
+++ elementry/script.js
@@ -5,26 +5,34 @@ angular.module('ILIGCapp', ['ngMaterial', 'ngMessages'])
       $scope.sparray = (new Function("return [" + $scope.sp + "];")());
       $scope.spsum = $scope.sparray.reduce((a, b) => a + b, 0);
       $scope.speaking = Number($scope.spsum / $scope.sparray.length) * 2;
+      console.log('sp: ' + $scope.speaking);
 
       $scope.rdarray = (new Function("return [" + $scope.rd + "];")());
       $scope.rdsum = $scope.rdarray.reduce((a, b) => a + b, 0);
       $scope.reading = Number($scope.rdsum / $scope.rdarray.length) * 2;
+      console.log('rd: ' + $scope.reading);
 
       $scope.lnarray = (new Function("return [" + $scope.ln + "];")());
       $scope.lnsum = $scope.lnarray.reduce((a, b) => a + b, 0);
       $scope.listening = Number($scope.lnsum / $scope.lnarray.length) * 2;
+      console.log('ln: ' + $scope.listening);
 
       $scope.wrarray = (new Function("return [" + $scope.wr + "];")());
       $scope.wrsum = $scope.wrarray.reduce((a, b) => a + b, 0);
       $scope.writing = Number($scope.wrsum / $scope.wrarray.length) * 1;
+      console.log('wr: ' + $scope.writing);
 
-      $scope.wr2array = (new Function("return [" + $scope.wr2 + "];")());
-      $scope.wr2sum = $scope.wr2array.reduce((a, b) => a + b, 0);
-      $scope.writing2 = Number($scope.wr2sum / $scope.wr2array.length) * 1;
+      $scope.wbarray = (new Function("return [" + $scope.wb + "];")());
+      $scope.wbsum = $scope.wbarray.reduce((a, b) => a + b, 0);
+      $scope.workbook = Number($scope.wbsum / $scope.wbarray.length) * 1;
+      console.log('wb: ' + $scope.workbook);
 
-      $scope.allaverage = $scope.speaking + $scope.reading + $scope.listening + $scope.writing + $scope.writing2;
+      $scope.allaverage = $scope.speaking + $scope.reading + $scope.listening + $scope.writing + $scope.workbook;
+      console.log('total: '+$scope.allaverage);
       $scope.pregrade = Number($scope.allaverage / 8).toFixed(2);
+      console.log('pre/8: ' + $scope.pregrade);
       $scope.finalgrade = $scope.pregrade * 20;
+      console.log('final: ' + $scope.finalgrade);
       
       // $mdToast.show(
       //   $mdToast.simple()
